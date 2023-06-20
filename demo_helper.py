59af6cabc8864acb047f5956cfbee6d6d14a71fa
acorstanje <a.corstanje@astro.ru.nl>
2023-06-20 10:56:13 +0200

    Pre-release version

---
+++ demo_helper.py
@@ -0,0 +1,125 @@
+# Separate module for cross-correlating and making a demo plot
+# Author: A. Corstanje, (a.corstanje@astro.ru.nl), 2023
+
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal as scipy_signal
+
+def get_freq_axis(signal):
+    # Return the frequency axis of a (real) FFT spectrum
+    # Frequencies in MHz (hence the 1.0e-6), assumed a 0.1 ns sample period
+    return (1.0e-6 * np.fft.rfftfreq(signal.shape[0], d=0.1e-9)) # MHz
+
+def do_filter_signal_lowpass(signal, cutoff_freq):
+    # For one antenna, signal shape (Nsamples)
+    # cutoff_freq in MHz
+
+    freqs = get_freq_axis(signal)
+
+    filter_indices = np.where(freqs > cutoff_freq)
+    spectrum = np.fft.rfft(signal)
+    spectrum[filter_indices] *= 0.0
+    signal_filtered = np.fft.irfft(spectrum)
+
+    return signal_filtered
+
+def get_crosscorrelation(test_signal, orig_signal, upsampling_factor=10):
+
+    orig_signal_upsampled = scipy_signal.resample(orig_signal, upsampling_factor*len(orig_signal) )
+    test_signal_upsampled = scipy_signal.resample(test_signal, upsampling_factor*len(test_signal) )
+
+    #orig_upsampled = scipy.signal.resample(orig_signal, upsampling_factor*len(orig_signal) )
+    #interp_upsampled = scipy.signal.resample(interpolated_signal, upsampling_factor*len(interpolated_signal) )
+
+    crosscorr = scipy_signal.correlate(test_signal_upsampled, orig_signal_upsampled)
+    #lags = signal.correlation_lags(orig_pulse.size, interpolated_pulse.size)
+    #    lag = lags[np.argmax(crosscorr)]
+    normalization = np.sqrt(np.sum(orig_signal_upsampled**2) * np.sum(test_signal_upsampled**2))
+    crosscorr /= normalization
+
+    autocorr = scipy_signal.correlate(orig_signal_upsampled, orig_signal_upsampled)
+    max_autocorr = np.argmax(autocorr) # this is at "t=0"
+
+    # Get Delta t, maximum at zero time shift, maximum overall
+    CC_optimized_timeshift = np.max(crosscorr)
+    CC_zeroshift = crosscorr[max_autocorr] # the CC value at fixed timing
+
+    delta_t = 0.1 * (1.0 / upsampling_factor) * (np.argmax(crosscorr) - np.argmax(autocorr)) # 0.1 ns per sample in original signal
+
+    # so delta_t is given in ns
+
+    orig_energy = np.sum(orig_signal_upsampled**2)
+    test_energy = np.sum(test_signal_upsampled**2)
+    energy_rel_diff = (test_energy - orig_energy) / orig_energy
+
+    return (CC_zeroshift, CC_optimized_timeshift, delta_t, energy_rel_diff)
+
+
+
+def plot_pulse_and_spectrum(orig_pulse, interpolated_pulse, x, y, cutoff_freq, pol):
+    # input pulses as 1D traces, i.e. shape (Nsamples)
+    # and position x, y in the footprint (in shower plane)
+    # x, y, cutoff freq, pol just for writing on the plot
+
+    time_axis = 0.1 * np.arange(len(orig_pulse))
+    radius = np.sqrt(x**2 + y**2)
+    freqs = get_freq_axis(orig_pulse)
+
+    (CC_zeroshift, CC_optimized_timeshift, delta_t, energy_rel_diff) = get_crosscorrelation(orig_pulse, interpolated_pulse)
+
+    fig, ax = plt.subplots(figsize=(10.67, 4), nrows=1, ncols=2)
+    ax1, ax2 = ax[0], ax[1]
+
+    #plt.figure()
+    ax1.plot(time_axis, 1.0e6 * orig_pulse, label='orig pulse', lw=2)
+    ax1.plot(time_axis, 1.0e6 * interpolated_pulse, label='interpolated pulse', lw=2)
+    residual = 1.0e6 * (interpolated_pulse - orig_pulse)
+    ax1.plot(time_axis, residual, label='difference', lw=2, c='g')
+
+    interpolated_energy = np.sum(interpolated_pulse**2)
+    orig_energy = np.sum(orig_pulse**2)
+    #fixed_pulse = interpolated_pulse * np.sqrt(orig_energy / interpolated_energy)
+    #plt.plot(time_axis, fixed_pulse, label='fixed pulse', c='r', lw=1)
+    ax1.grid()
+    ax1.set_xlabel('Time [ ns ]')
+    ax1.set_ylabel(r'E-field [ $\mu$V/m ]')
+    ax1.set_xlim(0.0, 50.0)
+    ax1.legend(loc='best')
+
+    #plt.figure()
+    orig_pulse_powerspec = np.abs(np.fft.rfft(orig_pulse))**2
+    interp_pulse_powerspec = np.abs(np.fft.rfft(interpolated_pulse))**2
+    ax2.plot(freqs, orig_pulse_powerspec, label='Orig pulse')
+    ax2.plot(freqs, interp_pulse_powerspec, label='Interpolated pulse')
+    ax2.text(0.98, 0.40, 'Position x = %3.1f, y = %3.1f, r = %3.2f m, pol = %d' % (x, y, radius, pol), transform=plt.gca().transAxes, ha='right') #, va='right')
+    ax2.text(0.98, 0.30, 'CC = %1.5f, CC_max = %1.5f' % (CC_zeroshift, CC_optimized_timeshift), transform=plt.gca().transAxes, ha='right')
+    ax2.text(0.98, 0.20, 'delta_t = %1.2f ns, cutoff freq = %3.1f MHz' % (delta_t, cutoff_freq), transform=plt.gca().transAxes, ha='right')
+
+    #plt.yscale('log')
+    ax2.grid()
+    ax2.set_xlabel('Frequency [ MHz ]')
+    ax2.set_ylabel('Power spectrum [ a.u. ]')
+    ax2.set_xlim(0, 500)
+    ax2.set_ylim(0.0, 1.2*np.max(orig_pulse_powerspec))
+    ax2.legend(loc='best')
+
+
+def read_data_hdf5(filename):
+    try:
+        demo_file = h5py.File(filename, 'r')
+    except:
+        raise ValueError('Cannot read data file; demo data downloaded with download_demo_data.sh?')
+    zenith = demo_file.get('zenith')[()]
+    azimuth = demo_file.get('azimuth')[()]
+    xmax = demo_file.get('xmax')[()]
+    footprint_positions = np.array(demo_file.get('footprint_positions'))
+    test_positions = np.array(demo_file.get('test_positions'))
+    (footprint_pos_x, footprint_pos_y) = (footprint_positions[:, 0], footprint_positions[:, 1])
+    (test_pos_x, test_pos_y) = (test_positions[:, 0], test_positions[:, 1])
+
+    footprint_antenna_data = np.array(demo_file.get('footprint_antennas'))
+    test_antenna_data = np.array(demo_file.get('test_antennas'))
+    demo_file.close()
+
+    return (zenith, azimuth, xmax, footprint_pos_x, footprint_pos_y, test_pos_x, test_pos_y, footprint_antenna_data, test_antenna_data)
---
+++ minimal_usage_demo.py
@@ -0,0 +1,48 @@
+# Minimal usage demo script of Fourier interpolation of pulse signals along simulated CR radio footprints
+# Author: A. Corstanje (a.corstanje@astro.ru.nl), 2023
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+import interpolation_Fourier
+import signal_interpolation_Fourier as sigF
+
+import demo_helper
+
+# For using the interpolator, the x and y positions of the simulated antennas in the shower plane are needed,
+# along with the time traces for all these antennas and polarizations
+# The antennas need not be ordered by position
+
+# Shapes are 1D array for x, y (in meters)
+# antenna traces 'footprint_antenna_data' in shape (Nant, Nsamples, Npol), i.e., in this example (208, 4082, 2)
+# Additional antenna positions have been simulated ('test_antenna_data' and 'test_pos_x', 'test_pos_y') to test the interpolation accuracy
+
+# When within < ~10 degrees from the North-South axis (including zenith), rotate the `on-sky` polarizations by 45 degrees
+# to avoid alignment with vx(vxB) and therefore having near-zero signals along circles in the footprint
+
+# Read in demo data
+demo_filename = 'demo_shower.h5'
+(zenith, azimuth, xmax, footprint_pos_x, footprint_pos_y, test_pos_x, test_pos_y, footprint_antenna_data, test_antenna_data) = demo_helper.read_data_hdf5(demo_filename)
+
+# Initialize the interpolator object. It needs the antenna positions (x and y as 1D arrays) and their time traces as 3D arrays (Nants, Nsamples, Npols)
+# By default the phase-interpolating method is "phasor" (see article)
+print('Initializing interpolator object...')
+phase_method = "phasor" # other option is "timing" which takes longer to initialize (to check which performs better in use cases not yet tested)
+signal_interpolator = sigF.interp2d_signal(footprint_pos_x, footprint_pos_y, footprint_antenna_data, verbose=False, phase_method=phase_method)
+
+test_index = 124 # take one of the 250 test positions
+pol = 0
+this_x, this_y = test_pos_x[test_index], test_pos_y[test_index]
+print('Interpolating pulse at position x = %3.2f, y = %3.2f m' % (this_x, this_y))
+
+orig_pulse = test_antenna_data[test_index] # shape is (Nsamples, Npol)
+
+# Call the interpolator object to obtain the interpolated pulse at the desired position
+# Return shape is (Nsamples, Npol)
+# Optionally, it can be low-pass filtered to an estimated reliable cutoff frequency
+# This is a reliable yet sometimes overly conservative estimate up to which frequency the interpolation is accurate
+interpolated_pulse = signal_interpolator(this_x, this_y) #, filter_up_to_cutoff=True)
+
+# Make a plot of the trace and spectrum
+demo_helper.plot_pulse_and_spectrum(orig_pulse[:, pol], interpolated_pulse[:, pol], this_x, this_y, -1, pol)
